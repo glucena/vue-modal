6898bf9afa31e0e439ae86dedd2ed92cd7d04856
glucena <gabriellucenatoro@gmail.com>
2018-01-31 23:41:15 +0100

    Implemented modal component and a Vue.js app to test it

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>Open modal POC</h1>
+    <modal v-show="isModalVisible" @close="closeModal" />
+    <button type="button"
+            class="btn"
+            @click="showModal">
+      Open Modal!
+    </button>
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data () {
+    return {
+      isModalVisible: false
+    }
+  },
+  methods: {
+    showModal () {
+      this.isModalVisible = true
+    },
+    closeModal () {
+      this.isModalVisible = false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
